0713dd655ab2ea250b749937d16c8c0f69af5f2d
Ben <benjaminafoster4@gmail.com>
2025-04-19 09:49:17 -0400

    file server alive with healthz endpoint

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/benjaminafoster/chirpy
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	
+
+
+	srv := &http.Server{
+		Addr:     ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+
+}
