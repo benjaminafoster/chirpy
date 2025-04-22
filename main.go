33ab12cc25d7a172128644def64b430c8fd08945
Ben <benjaminafoster4@gmail.com>
2025-04-22 19:07:58 -0400

    completed initial database setup

--- main.go
+++ main.go
@@ -4,10 +4,19 @@ import (
 	"log"
 	"net/http"
 	"sync/atomic"
+	"os"
+	"database/sql"
+	"fmt"
+	"github.com/benjaminafoster/chirpy/internal/database"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
 
 type apiConfig struct {
 	fileserverHits  atomic.Int32
+	dbQPtr       *database.Queries
 }
 
 
@@ -15,8 +24,18 @@ type apiConfig struct {
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
 
-	apiCfg := apiConfig{}
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("error connecting to postgres DB: %s", err)
+		os.Exit(1)
+	}
+
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}, dbQPtr: dbQueries}
 
 	fileserverHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 
@@ -26,7 +45,6 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
-	// Add /api/validate_chirp
 	
 
 
